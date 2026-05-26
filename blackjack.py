59ce732a33be79f8a9b72a09c9847e6cf5520aad
KarolBurrola <karolburrola02@gmail.com>
2026-05-26 12:49:59 -0700

    Correcciones, implementación de funciones auxiliares, definiendo estructura base del BJ

--- blackjack.py
+++ blackjack.py
@@ -12,35 +12,98 @@ class BlackJack(MDPsim):
     """
     def __init__(self, gama):
         """
-        Inicializando variables
+        Inicializando
         """
-        self.estados = []
-        for suma in range(12, 22):
-            for carta_crupier in range(1, 11):
-                for as_usable in [True, False]:
-                    estado = (suma, carta_crupier, as_usable)
-                    self.estados.append(estado)
+        self.estados = [
+            (s, c, a)
+            for s in range(12, 22)
+            for c in range(1, 11)
+            for a in [True, False]
+        ]
+        self.mazo_player = []
+        self.mazo_crupier = []
+        self.natural = False
+        super().__init__(self.estados, gama)
 
-        self.gama = gama
-        self.carta_c_oculta = None
-        
+    def reparte_carta(self):
+        """
+        Función auxiliar que devuelve un valor aleatorio entre 1 y 10 (con probabilidad 4/13
+        para el valor 10)
+        """
+        carta = randint(1, 13)
+        return 10 if carta > 10 else carta
+
     def estado_inicial(self):
-        # TODO: implementar el estado inicial del blackjack
-        raise NotImplementedError("Implementa el estado inicial del blackjack")
-    
+        """
+        Reinicia el mazo y reparte dos cartas al jugador y dos al crupier (una oculta).
+        Calcula el estado inicial, verifica si hay Blackjack natural inmediato (suma
+        == 21 con las primeras dos cartas iniciales).
+        """
+        self.mazo_player = []
+        self.mazo_crupier = []
+        self.natural = False
+
+        self.mazo_player = self.agregar_carta(self.mazo_player, self.reparte_carta())
+        self.mazo_player = self.agregar_carta(self.mazo_player, self.reparte_carta())
+        self.mazo_crupier = self.agregar_carta(self.mazo_crupier, self.reparte_carta())
+        self.mazo_crupier = self.agregar_carta(self.mazo_crupier, self.reparte_carta(), visible=False)
+
+        as_u = self.as_usable()
+        suma_jugador = self.sumar_mazo(self.mazo_player)
+
+        if suma_jugador == 21:
+            self.natural = True
+
+        return (suma_jugador, self.obtener_carta_crupier(), as_u)
+
+    def as_usable(self):
+        """
+        Contabiliza el As como 11 si no excede 21. Si la suma excede 21 y se tiene un As de valor 11,
+        lo transforma automáticamente a valor 1 y cambia el estado a As Usable = False.
+        Retorna True si hay un as usable (contado como 11), False en caso contrario.
+        """
+        index_ases = [i for i, (c, v) in enumerate(self.mazo_player) if c == 1 or c == 11]
+        suma_jugador = self.sumar_mazo(self.mazo_player)
+
+        if suma_jugador > 21:
+            for i in index_ases:
+                c, v = self.mazo_player[i]
+                if c == 11:
+                    self.mazo_player[i] = (1, v)
+                    return False
+        else:
+            for i in index_ases:
+                c, v = self.mazo_player[i]
+                if c == 1 and suma_jugador + 10 <= 21:
+                    self.mazo_player[i] = (11, v)
+                    return True
+
+        return False
+
+    def agregar_carta(self, mazo, c1, visible=True):
+        mazo.append((c1, visible))
+        return mazo
+
+    def sumar_mazo(self, mazo):
+        return sum([c for c, v in mazo])
+
+    def obtener_carta_crupier(self):
+        c, v = self.mazo_crupier[0]
+        return c
+
     def acciones_legales(self, s):
         plantarse = 0
         pedir = 1
         return [plantarse, pedir]
-    
+
     def recompensa(self, s, a, s_):
         # TODO: implementar la recompensa del blackjack
         raise NotImplementedError("Implementa la recompensa del blackjack")
-    
+
     def transicion(self, s, a):
         # TODO: implementar la transición del blackjack
         raise NotImplementedError("Implementa la transición del blackjack")
-    
+
     def es_terminal(self, s):
         # TODO: implementar la condición de estado terminal del blackjack
         raise NotImplementedError("Implementa la condición de estado terminal del blackjack")
@@ -48,27 +111,26 @@ class BlackJack(MDPsim):
 
 if __name__ == "__main__":
 
-    blackjack = BlackJack(gama=1,...) # TODO: agregar los parámetros necesarios para el blackjack   
+    blackjack = BlackJack(gama=1, ...)  # TODO: agregar los parámetros necesarios para el blackjack
 
-    # TODO: definir los parámetros de SARSA y Q-learning, luego crear las instancias 
+    # TODO: definir los parámetros de SARSA y Q-learning, luego crear las instancias
     # de cada algoritmo
-    Q_sarsa = SARSA( blackjack, alfa=..., epsilon=..., n_ep=..., n_iter=...)
-    Q_learning = Q_learning( blackjack, alfa=..., epsilon=..., n_ep=..., n_iter=...)
+    Q_sarsa = SARSA(blackjack, alfa=..., epsilon=..., n_ep=..., n_iter=...)
+    Q_learning = Q_learning(blackjack, alfa=..., epsilon=..., n_ep=..., n_iter=...)
 
     # Encuentra las políticas óptimas para cada algoritmo
     pi_s = PoliticaGreedy(Q_sarsa)
     pi_q = PoliticaGreedy(Q_learning)
 
     # Imprime las políticas óptimas para cada estado no terminal
-    print("Estado".center(10) + '|' +  "SARSA".center(10) + '|' + "Q-learning".center(10))
-    print("-"*10 + '|' + "-"*10 + '|' + "-"*10)
+    print("Estado".center(10) + '|' + "SARSA".center(10) + '|' + "Q-learning".center(10))
+    print("-" * 10 + '|' + "-" * 10 + '|' + "-" * 10)
     for s in blackjack.estados:
         if not blackjack.es_terminal(s):
-            print(str(s).center(10) + '|' 
-                  + str(pi_s(s)).center(10) + '|' 
+            print(str(s).center(10) + '|'
+                  + str(pi_s(s)).center(10) + '|'
                   + str(pi_q(s)).center(10))
-    print("-"*10 + '|' + "-"*10 + '|' + "-"*10)
-
+    print("-" * 10 + '|' + "-" * 10 + '|' + "-" * 10)
 
 """
 ****************************************************************************************
